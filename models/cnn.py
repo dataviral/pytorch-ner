d16b18ad7fd555ffc304110fdcda81a8a1893e66
Joshi, Aviral <aviral.joshi@jpmchase.com>
2022-01-19 12:38:18 +0530

    cnn_ner

---
+++ models/cnn.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CnnNER(nn.Module):
+
+    def __init__(
+        self,
+        n_classes,
+        n_embeddings,
+        embed_dims,
+        n_cnn_layers,
+        n_cnn_channels=32
+    ):
+        super(CnnNER, self).__init__()
+
+        """model hyperparameters"""
+        self.n_classes = n_classes
+        self.n_embeddings = n_embeddings
+        self.embed_dims = embed_dims
+        self.n_cnn_layers = n_cnn_layers
+        self.n_cnn_channels = n_cnn_channels
+
+        """model layers"""
+        # input embedding layer
+        self.embedding_layer = nn.Embedding(
+            num_embeddings=n_embeddings,
+            embedding_dim=embed_dims
+        )
+
+        # cnn layers
+        cnn_layers_list = [
+            nn.Sequential(
+                nn.Conv1d(
+                    in_channels=embed_dims if i == 0 else n_cnn_channels,
+                    out_channels=n_cnn_channels,
+                    kernel_size=3,
+                    padding=1,
+                    bias=False,
+                ),
+                nn.BatchNorm1d(n_cnn_channels),
+                nn.ReLU()
+            )
+            for i in range(n_cnn_layers)
+        ]
+        self.cnn_layers = nn.Sequential(*cnn_layers_list)
+
+        # output projection layer
+        self.output_layer = nn.Linear(n_cnn_channels, n_classes)
+
+    def forward(self, x, train=False):
+        # generate embeddings
+        embeds = self.embedding_layer(x)  # (B, T) => (B, T, EMBED_DIM)
+        embeds = embeds.transpose(2, 1) # (B, T, EMBED_DIM) => (B, EMBED_DIM, T)
+
+        # convolve
+        cnn_op = self.cnn_layers(embeds)  # (B, EMBED_DIM, T) => (B, CNN_CHANNELS, T)
+        cnn_op = cnn_op.transpose(2, 1) # (B, CNN_CHANNELS, T) => (B, T, CNN_CHANNELS)
+
+        # project
+        op_logits = self.output_layer(cnn_op)  # (B, T, CNN_CHANNELS) => (B, T, N_CLASSES)
+
+        if train:
+            return op_logits
+        else:
+            op = torch.argmax(F.softmax(op_logits, dim=-1), dim=-1)  # (B, T, N_CLASSES) => (B ,T)
+            return op
+
+
+if __name__ == "__main__":
+    B, T = 32, 100
+    model_config = {
+        "n_classes": 10,
+        "n_embeddings": 10000,
+        "embed_dims": 69,
+        "n_cnn_layers": 5,
+        "n_cnn_channels": 12
+    }
+
+    inputs = torch.randint(
+        low=0,
+        high=model_config["n_embeddings"],
+        size=(B, T)
+    )
+    expected_output_shape = (B, T, model_config["n_classes"])
+
+    model = CnnNER(**model_config)
+    outputs = model(inputs, train=True)
+
+    assert outputs.shape == expected_output_shape
+    print(f"input: {inputs.shape})")
+    print(f"model config: ", model_config)
+    print(f"output: {outputs.shape})")
